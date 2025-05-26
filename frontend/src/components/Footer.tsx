67685ea289b6f1a8b6c94d2f16db4047c60a8c2f
ShlomiTal <68659928+ShlomiTal@users.noreply.github.com>
2025-05-26 23:03:18 +0300

    Add files via upload

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background: #2a2a2a;
+  padding: 10px;
+  text-align: center;
+  color: #cccccc;
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <p>© 2025 Argento. All rights reserved.</p>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
