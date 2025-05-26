80bef435479eaa39b9c6a9549d426b68417f7abb
ShlomiTal <68659928+ShlomiTal@users.noreply.github.com>
2025-05-26 14:32:29 +0300

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
-import { registerSW } from './serviceworker';
-
-
-const port = process.env.PORT || 8080;
+import { registerSW } from './serviceWorker'; // Corrected case
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
